ba58648daa560d71b321d613c3df766cc7b3fdc2
Mariya Ivanova <m80ivanova@gmail.com>
2021-03-18 18:33:12 -0500

    Navigation And Home Component

---
+++ shared/featuredLunches.js
@@ -0,0 +1,34 @@
+export const LUNCHES = [
+  {
+    id: 0,
+    name: "Chicken and Rice Soup",
+    featured: true,
+    servings: "6",
+    image: "/assets/images/chicken-rice-soup.jpg",
+    ingredients:
+      "1/3 cup brown rice 4 cups, chicken broth, 2 pieces of ginger, 1 clove garlic, 1 lb. boneless and skinless chicken thighs, 1 tsp salt,  1 cup diced carrots,  1 cup diced celery, 1/2 cup chopped parsley",
+    directions: [
+      "1. In a large pot, bring the rice, stock, ginger and garlic to a boil. Lower the heat to medium and cook for about 15 minutes.",
+      " 2. Add in the chicken, salt, carrots and celery. Bring back to a boil, lower to a simmer, cover the pot and cook for 20 minutes. ",
+      "3. Remove chicken from the pot and put aside until cool enough to handle. ",
+      "4. Continue to cook the rice and vegetables in the broth until rice is fully cooked, about 20 more minutes.  ",
+      " 5. Meanwhile, dice the chicken into bite-size pieces. Once the rice is fully cooked, return the chicken to the pot and cook long enough to heat the chicken through. Serve sprinkled with the parsley.",
+    ],
+  },
+  {
+      id: 1,
+      name: "5-Ingredient Cheese Naan Pizzas",
+      featured: false,
+      servings: "4",
+      image: "",
+      ingredients: " 4 pieces naan, 2tsp olive oil, 2/3 cup pizza sauce, 1 1/2 cups mozzarella cheese",
+      directions: [
+          "1. Preheat oven to 425F.",
+          "2. Line a baking sheet with parchment paper.",
+          "3. Arrange the naan on the baking sheet. Drizzle an equal amount of olive oil over the surface of each piece.",
+          "4. Divide the pizza sauce equally between the naan pieces and spread into an even layer, leaving a 1/2-inch border around the sauce.",
+          "5. Sprinkle the cheese amd dried oregano in equal amounts on each naan piece.",
+          "6. Bake pizzas on the middle rack of oven until the naan is crisp and the cheese is bubbly and golden, 12-14 minutes.",
+      ]
+  }
+];
